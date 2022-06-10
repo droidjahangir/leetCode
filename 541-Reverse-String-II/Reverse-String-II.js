39ad62685ca107b45bb0dbbcad15cb3d1f50b0df
jahangir.droid@gmail.com <jahangir.droid@gmail.com>
2022-06-10 11:09:40 +0600

    Runtime: 68 ms

--- 541-Reverse-String-II/Reverse-String-II.js
+++ 541-Reverse-String-II/Reverse-String-II.js
@@ -37,3 +37,33 @@ var reverseStr = function(s, k) {
     return finalResult.join('')
 
 };
+
+/*
+--- Most convenient solution ---
+var reverseStr = function(s, k) {
+    if (s.length === 1) return s;
+    else if (s.length < k)
+        return s.split('').reverse().join('');
+    else if (s.length <= 2 * k){
+        let rev = s.slice(0, k).split('').reverse().join('');
+        return rev + s.slice(k);
+    }
+
+
+    for (let i = 0; i < s.length;){
+        let str = s.slice(i, 2 * k + i);
+        if (str.length <= k) {
+            str = str.split('').reverse().join('');
+        }
+        else {
+            let start = s.slice(i, k + i).split('').reverse().join('');
+            str = start + str.slice(k);
+        }
+        s = s.slice(0, i) + str + s.slice(i + 2 * k);
+
+        i += 2 * k;
+    }
+    return s;
+}
+
+*/
