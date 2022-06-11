b807dbf36ebf1c76da4f864dc43c780383b49da0
jahangir.droid@gmail.com <jahangir.droid@gmail.com>
2022-06-11 17:05:13 +0600

    Array [ Hard ] - faster than 31.32% of JavaScript online submissions

--- 41-First-Missing-Positive/First-Missing-Positive.js
+++ 41-First-Missing-Positive/First-Missing-Positive.js
@@ -1,6 +1,22 @@
 var firstMissingPositive = function(nums) {
-    // nums.sort((a,b) => a - b)
-    console.log(nums.sort((a,b) => a - b))
+    let map = {}
+    nums.sort((a,b) => a - b)
+    for (let i = 0; i < nums.length; i++) {
+        if (nums[i] < 0) {
+            nums.shift()
+            i--
+        }
+    }
+    for (let i = 0; i < nums.length; i++) {
+        if (map[nums[i]] === undefined) {
+            map[nums[i]] = i
+        }
+    }
+    for (let i = 0; i <= nums.length; i++) {
+        if (map[i+1] === undefined){
+            return i+1
+        }
+    }
 };
 
-firstMissingPositive([3,4,-1,1])
+console.log(firstMissingPositive([3,-7,4,-1,2,1,-3]))
