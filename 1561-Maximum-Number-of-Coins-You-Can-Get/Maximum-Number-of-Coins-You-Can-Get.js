fd6e05da071512a20265200383524b914be84651
jahangir.droid@gmail.com <jahangir.droid@gmail.com>
2022-06-11 15:50:49 +0600

    Array [ Medium ] - faster than 90.99% of JavaScript online submissions

---
+++ 1561-Maximum-Number-of-Coins-You-Can-Get/Maximum-Number-of-Coins-You-Can-Get.js
@@ -0,0 +1,17 @@
+var maxCoins = function(piles) {
+    piles.sort(function(a, b) {
+        return a - b;
+    })
+    let count = 0
+    let arr = []
+    for (let i = 0, j = piles.length-1; i < piles.length/3; i++, j -= 2) {
+        arr.push(piles[j])
+        arr.push(piles[j-1])
+        arr.push(piles[i])
+        count += arr[1]
+        arr = []
+    }
+    return count
+};
+
+console.log(maxCoins([9,5,6,8,10,1,4,10,7]))
