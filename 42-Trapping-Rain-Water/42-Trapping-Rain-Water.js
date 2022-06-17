05e82061591586007d2f5d2ece36d25797516136
jahangir.droid@gmail.com <jahangir.droid@gmail.com>
2022-06-17 20:22:20 +0600

    Array(Two pointer)-[Hard]-Runtime: 81 ms, faster than 75.89% of JavaScript online submissions

---
+++ 42-Trapping-Rain-Water/42-Trapping-Rain-Water.js
@@ -0,0 +1,28 @@
+const trap = function (height) {
+    let left = 0
+    let right = height.length - 1
+    let totalWater = 0
+    let maxLeft = 0
+    let maxRight = 0
+
+    while (left < right) {
+        if (height[left] <= height[right]) {
+            if (height[left] >= maxLeft) {
+                maxLeft = height[left]
+            } else {
+                totalWater += maxLeft - height[left]
+            }
+            left++
+        } else {
+            if (height[right] >= maxRight) {
+                maxRight = height[right]
+            } else {
+                totalWater += maxRight - height[right]
+            }
+            right--
+        }
+    }
+    return totalWater
+}
+
+console.log(trap([0,1,0,2,1,0,1,3,2,1,2,1]))
