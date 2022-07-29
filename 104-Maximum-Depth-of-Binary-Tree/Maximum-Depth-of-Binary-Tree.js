d6bd46ec75bb02e1aadef4db0965a72bbdf2c7bf
jahangir.droid@gmail.com <jahangir.droid@gmail.com>
2022-07-29 12:52:10 +0600

    Binary tree [Easy] - 64 ms, faster than 98.69% of JavaScript online submissions

---
+++ 104-Maximum-Depth-of-Binary-Tree/Maximum-Depth-of-Binary-Tree.js
@@ -0,0 +1,7 @@
+const maxDepth = function (node, currentDepth){
+    if (!node){
+        return currentDepth
+    }
+    currentDepth++
+    return Math.max(maxDepth(node.left, currentDepth), maxDepth(node.right, currentDepth))
+}
---
+++ 104-Maximum-Depth-of-Binary-Tree/README.md
@@ -0,0 +1,3 @@
+leetcode problem link here ....
+
+[104. Maximum Depth of Binary Tree](https://leetcode.com/problems/maximum-depth-of-binary-tree/)
