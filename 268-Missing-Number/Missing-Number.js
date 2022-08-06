13740d92255fcfffc77fc334d8d2e9759349f669
jahangir.droid@gmail.com <jahangir.droid@gmail.com>
2022-08-07 00:08:15 +0600

    Search [Easy] - 87 ms, faster than 79.86% of JavaScript online submissions

---
+++ 268-Missing-Number/Missing-Number.js
@@ -0,0 +1,8 @@
+var missingNumber = function(nums) {
+    const res = new Array(nums.length+1).fill(-1);
+
+    for(const num of nums) {
+        res[num] = num;
+    }
+    return res.indexOf(-1);
+};
---
+++ 268-Missing-Number/README.md
@@ -0,0 +1,3 @@
+Leetcode problem link
+
+[268. Missing Number](https://leetcode.com/problems/missing-number/)
