29888b4931c2845087dd783e071a985b8db3fa00
jahangir.droid@gmail.com <jahangir.droid@gmail.com>
2022-08-06 19:39:03 +0600

    Queue [Medium] 86ms, beats 87.81 % of javascript submissions

--- 2089-Find-Target-Indices-After-Sorting-Array/Find-Target-Indices-After-Sorting-Array.js
+++ 2089-Find-Target-Indices-After-Sorting-Array/Find-Target-Indices-After-Sorting-Array.js
@@ -21,10 +21,6 @@
 //     return result.sort((a,b) => a-b);
 // }
 
-
-
-
-
 const binarySearch = function (nums, left, right, target) {
     while (left <= right) {
         const mid = Math.floor((left+right)/2)
